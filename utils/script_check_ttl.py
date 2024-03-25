9dc2abd2c9e7f63a7532133e4b9da6230b3f2628
Murasambrero <neones@yandex.ru>
2024-03-25 22:35:48 +0800

    update script_check_ttl

--- utils/script_check_ttl.py
+++ utils/script_check_ttl.py
@@ -1,6 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from core.models import SecretModel
+from core.crud import delete_secret
 from core.database import get_session
 import datetime
 
@@ -9,5 +11,13 @@ async def remove_secrets_by_ttl(
     db: AsyncSession = Depends(get_session),
 ):  # удаление секретов с истекшим сроком действия
     current_time = datetime.datetime.now(datetime.timezone.utc)
-    query = db.query(SecretModel).filter(SecretModel.elapsed_at < current_time)
-    await query.delete(synchronize_session=False)
+    secrets: list[SecretModel] = await db.execute(
+        select(SecretModel).where(
+            SecretModel.elapsed_at < current_time
+        )
+    )
+    for secret in secrets.scalars().all():
+        if (
+            datetime.datetime.now(datetime.timezone.utc) > secret.elapsed_at
+        ):  # проверка истечения срока действия секрета
+            await delete_secret(secret, db)
